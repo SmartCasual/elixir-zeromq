4927f324ae64ed573af9959d2dc70369b3e52b0e
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2016-05-21 23:50:30 +0100

    Add a frame splitter.
    Takes arbitrary amounts of binary over arbitrary
    calls, building up a queue of complete frames which
    can be retrieved later.

---
+++ lib/util/frame_splitter.ex
@@ -0,0 +1,85 @@
+defmodule ElixirZeroMQ.FrameSplitter do
+  @moduledoc """
+  A GenServer which when fed a stream of binaries
+  will split out ZeroMQ frames and return the binary
+  blob (without parsing into a Command or Message).
+  """
+
+  use GenServer
+
+  @doc """
+  Starts the splitter.
+  """
+  def start_link do
+    GenServer.start_link(__MODULE__, :ok, [])
+  end
+
+  @doc """
+  Adds the provided binary blob to the current stream.
+  Returns `{:ok, flags, frame_blob}` if the full frame is available,
+  otherwise requests more data by returning `{:ok, :more}`.
+  """
+  def add_binary(splitter, blob) do
+    GenServer.call(splitter, {:add_binary, blob})
+  end
+
+  @doc """
+  Returns the oldest frame body and flags as `{:ok, flags, frame_body, remaining_count}`
+  if available, otherwise returns `:empty`.
+  """
+  def fetch(splitter) do
+    GenServer.call(splitter, :fetch)
+  end
+
+  @doc """
+  Initializes the state with a nil size & flags, the empty stream in progress
+  and the list for parsed frame bodies.
+  """
+  def init(:ok) do
+    {:ok, {nil, nil, <<>>, :queue.new}}
+  end
+
+  def handle_call({:add_binary, blob}, _from, {size, flags, stream, frame_bodies}) do
+    stream = stream <> blob
+
+    {flags, size, stream, frame_bodies} = extract_frame_body(flags, size, stream, frame_bodies)
+
+    {:reply, {:ok, :queue.len(frame_bodies)}, {size, flags, stream, frame_bodies}}
+  end
+
+  def handle_call(:fetch, _from, {size, flags, stream, frame_bodies}) do
+    case :queue.out(frame_bodies) do
+      {{:value, frame_body_and_flags}, frame_bodies} ->
+        {:reply, {:ok, frame_body_and_flags, :queue.len(frame_bodies)}, {size, flags, stream, frame_bodies}}
+      {:empty, frame_bodies} ->
+        {:reply, :empty, {size, flags, stream, frame_bodies}}
+    end
+  end
+
+  defp extract_frame_body(flags, size, stream, frame_bodies) do
+    if size == nil || flags == nil do
+      working_parts = ElixirZeroMQ.Frame.extract_flags_and_size(stream)
+    else
+      working_parts = {flags, size, stream}
+    end
+
+    if working_parts == :error do
+      if byte_size(stream) == 0 do
+        {nil, nil, stream, frame_bodies}
+      else
+        {flags, size, stream, frame_bodies}
+      end
+    else
+      {flags, size, stream} = working_parts
+
+      if byte_size(stream) >= size do
+        <<frame_body::binary-size(size), stream::binary>> = stream
+        frame_bodies = :queue.in({flags, frame_body}, frame_bodies)
+
+        extract_frame_body(nil, nil, stream, frame_bodies)
+      else
+        {flags, size, stream, frame_bodies}
+      end
+    end
+  end
+end
---
+++ test/util/frame_splitter_test.exs
@@ -0,0 +1,133 @@
+defmodule ElixirZeroMQ.FrameSplitterTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    short_frame = <<0x00, 10, "0123456789">>
+    long_frame = <<0x02, byte_size(loads_of_text)::8 * 8, loads_of_text::binary>>
+
+    {:ok, splitter} = ElixirZeroMQ.FrameSplitter.start_link
+
+    {:ok,
+      splitter: splitter,
+      short_frame: short_frame,
+      long_frame: long_frame,
+    }
+  end
+
+  test "works if provided an exact frame", context do
+    {:ok, 1} = ElixirZeroMQ.FrameSplitter.add_binary(
+      context[:splitter],
+      context[:short_frame]
+    )
+
+    {:ok, {flags, frame_body}, 0} = ElixirZeroMQ.FrameSplitter.fetch(context[:splitter])
+
+    assert flags == %{
+      command: false,
+      long: false,
+      more: false,
+    }
+
+    assert frame_body == "0123456789"
+  end
+
+  test "works if provided a long frame", context do
+    {:ok, 1} = ElixirZeroMQ.FrameSplitter.add_binary(
+      context[:splitter],
+      context[:long_frame]
+    )
+
+    {:ok, {flags, frame_body}, 0} = ElixirZeroMQ.FrameSplitter.fetch(context[:splitter])
+
+    assert flags == %{
+      command: false,
+      long: true,
+      more: false,
+    }
+
+    assert frame_body == loads_of_text
+  end
+
+  test "works if provided a frame over multiple calls", context do
+    <<first_part::binary-size(5), second_part::binary>> = context[:short_frame]
+
+    {:ok, 0} = ElixirZeroMQ.FrameSplitter.add_binary(context[:splitter], first_part)
+    {:ok, 1} = ElixirZeroMQ.FrameSplitter.add_binary(context[:splitter], second_part)
+
+    {:ok, {flags, frame_body}, 0} = ElixirZeroMQ.FrameSplitter.fetch(context[:splitter])
+
+    assert flags == %{
+      command: false,
+      long: false,
+      more: false,
+    }
+
+    assert frame_body == "0123456789"
+  end
+
+  test "providing multiple frames over multiple calls", context do
+    {:ok, 1} = ElixirZeroMQ.FrameSplitter.add_binary(context[:splitter], context[:short_frame])
+    {:ok, 2} = ElixirZeroMQ.FrameSplitter.add_binary(context[:splitter], context[:long_frame])
+
+    {:ok, {flags, frame_body}, 1} = ElixirZeroMQ.FrameSplitter.fetch(context[:splitter])
+
+    assert flags == %{
+      command: false,
+      long: false,
+      more: false,
+    }
+    assert frame_body == "0123456789"
+
+    {:ok, {flags, frame_body}, 0} = ElixirZeroMQ.FrameSplitter.fetch(context[:splitter])
+
+    assert flags == %{
+      command: false,
+      long: true,
+      more: false,
+    }
+
+    assert frame_body == loads_of_text
+  end
+
+  test "providing multiple frames over one call", context do
+    combined_frames = context[:short_frame] <> context[:long_frame]
+    {:ok, 2} = ElixirZeroMQ.FrameSplitter.add_binary(context[:splitter], combined_frames)
+
+    {:ok, {flags, frame_body}, 1} = ElixirZeroMQ.FrameSplitter.fetch(context[:splitter])
+
+    assert flags == %{
+      command: false,
+      long: false,
+      more: false,
+    }
+    assert frame_body == "0123456789"
+
+    {:ok, {flags, frame_body}, 0} = ElixirZeroMQ.FrameSplitter.fetch(context[:splitter])
+
+    assert flags == %{
+      command: false,
+      long: true,
+      more: false,
+    }
+
+    assert frame_body == loads_of_text
+  end
+
+  test ".fetch returns `:empty` if no completed frame bodies available", context do
+    result = ElixirZeroMQ.FrameSplitter.fetch(context[:splitter])
+
+    assert result == :empty
+  end
+
+  defp loads_of_text do
+    """
+    Sed posuere consectetur est at lobortis. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo. Cras mattis consectetur purus sit amet fermentum.
+
+    Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur. Curabitur blandit tempus porttitor. Donec sed odio dui. Curabitur blandit tempus porttitor. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+
+    Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna.
+
+    Curabitur blandit tempus porttitor. Nullam quis risus eget urna mollis ornare vel eu leo. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nullam quis risus eget urna mollis ornare vel eu leo.
+    """
+  end
+end
